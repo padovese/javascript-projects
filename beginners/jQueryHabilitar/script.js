fedad052ad3bc60b15180e4685d007de1eb5b70f
Padovese <bhspadoveze@gmail.com>
2018-01-17 22:26:17 -0200

    adding jQuery tests

---
+++ beginners/jQueryHabilitar/script.js
@@ -0,0 +1,14 @@
+	 	$(document).ready(function(){
+	 		$("#alerta").click(function(){
+	 			$("#btn-teste").hide();
+	 		});
+	 		$("#btn-b").click(function(){
+	 			$("#pop").hide();
+	 		});
+	 		$("#btn-a").click(function(){
+	 			if(false){
+	 				$("#pop").show();
+	 			}
+	 		});
+	 	});
+

2bedca94ed68b601b575423d89398e06c33093a5
Bruno Henrique Santana Padoves <bhsjnfw@CORP1.RC.ITAU>
2018-01-22 17:26:40 -0200

    adding some new effects on images

---
+++ beginners/jQuery dynamic images/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".childone").click(function(){
+        //$("#myImage").hide();
+        $("#myImage").hide(2000);
+    });
+    $(".childtwo").click(function(){
+        //$("#myImage").show();
+        $("#myImage").show(5000);
+    });
+});

e1ae8f8b520174803c22c3b84fbd53252689c0e1
padovese <bhspadoveze@gmail.com>
2019-09-11 21:13:19 -0300

    feature: parameters on directives

--- angularjs/src/main/resources/static/app.js
+++ angularjs/src/main/resources/static/app.js
@@ -14,7 +14,12 @@ app.config(function ($routeProvider) {
 });
 
 app.controller('aController', function ($scope){
-
+	$scope.firstName = 'Mickey';
+	$scope.lastName = 'Mouse';
+	$scope.person = {
+		name: 'Bruce',
+		lastName: 'Wayme'
+	}
 });
 
 app.controller('bController', function ($scope) {
@@ -23,7 +28,12 @@ app.controller('bController', function ($scope) {
 
 app.directive("someForm", function(){
 	return{
-		template: '<form>First name:<br><input type="text" name="firstname" value="Mickey"><br>Last name:<br><input type="text" name="lastname" value="Mouse"><br><br><input type="submit" value="Submit"></form>'
+		templateUrl: 'someForm.html',
+		scope: { 
+			firstName: '@', //accept text
+			lastName: '@',
+			person: "=" //accept object
+		}
 	}
 
 });
